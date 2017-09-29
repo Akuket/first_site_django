b6134cb86b1cb45edae2ea0b933dca19db5731bf
Guillaume <guillaume.cassou@supinfo.com>
2017-09-29 16:08:40 +0200

    changes

--- mysite/mysite/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^register/', include("register.urls")),
-    url(r'^admin/', admin.site.urls),
-]
--- mysite/register/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    is_subscriber = models.BooleanField(default=False)
-    date_end_subscription = models.DateTimeField('End of subscription')
-
-
