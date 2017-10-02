805d9dc5a0d719180f81453a0fa6ba9d62d4feff
akuket <guillaume.cassou@orange.fr>
2017-10-02 11:04:16 +0000

    initial commit

--- association/account/email.py
+++ association/account/email.py
@@ -6,13 +6,13 @@ def send_register_mail(link, name, email, association):
     subject = "Your ask for registration on %s." % association
     context = {'link_id': link, 'name': name}
     template = render_to_string("account/register_mail.html", context)
-    sender = ["guillaume.cassou@supinfo.com"]
-    EmailMessage(subject, template, to=[email], from_email=sender).send()
+    # sender = ["guillaume.cassou@supinfo.com"]
+    EmailMessage(subject, template, to=[email]).send()
 
 
 def send_reset_password_mail(link, name, email, association):
     subject = "Your ask for reset your password on %s" % association
     context = {'link_id': link, 'name': name}
     template = render_to_string("account/forgot_password__mail.html", context)
-    sender = ["guillaume.cassou@supinfo.com"]
-    EmailMessage(subject, template, to=[email], from_email=sender).send()
+    # sender = ["guillaume.cassou@supinfo.com"]
+    EmailMessage(subject, template, to=[email]).send()
--- association/payment/views.py
+++ association/payment/views.py
@@ -1,10 +1,13 @@
-from django.http.response import Http404
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
 
 from . api_payplug import Payment
 from . models import Subscription
+from account.api import accreditation_view_required
 
 
+@accreditation_view_required(redirect_url=reverse_lazy(u'login')) # si authentifié et email valide
 def subscription_view(request):
     template_name = "payment/subscription.html"
     try:
@@ -16,15 +19,15 @@ def subscription_view(request):
             'subscriptions': subscriptions
         })
 
-
+@accreditation_view_required(redirect_url=reverse_lazy(u'login')) # si authentifié et email valide
 def payment_view(request, subscription, product):
     # ne fonctionne pas sur une adresse locale
-    # return_url = request.build_absolute_uri(reverse("dashboard"))
-    # cancel_url = request.build_absolute_uri(reverse("dashboard"))
-    # hosted_payment = {'return_url': return_url, 'cancel_url': cancel_url}
+    return_url = request.build_absolute_uri(reverse("dashboard"))
+    cancel_url = request.build_absolute_uri(reverse("subscriptions"))
+    hosted_payment = {'return_url': return_url, 'cancel_url': cancel_url}
     subscription_object = Subscription.objects.get(name=subscription)
     for products in subscription_object.products.all():
         if products.name == product:
-            payment = Payment(request.user, product=products)
-    return payment.redirect_payment() or Http404
-
+            payment = Payment(request.user, product=products, payment_infos={'hosted_payment': hosted_payment})
+            return payment.redirect_payment()
+    return HttpResponseNotFound('<h1>Http 404</h1>')
